cca50d009849ba2a2053f7022bb26ec5bebdb950
Agil77 <108315883+Agil77@users.noreply.github.com>
2022-10-14 21:45:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -58,8 +58,8 @@ global.lolkey = '8e66d0934cf741bfd2182c16' // PUNYA WAHID 357d3c85079c4e0818731e
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['628XXXXXXXXXX', 'WH MODS DEV', true],
-  ['-', 'isi_kalau_ada', true],
+  ['6283144700823', 'BANG AL', true],
+  ['-', 'tes', true],
   ['-', 'isi_kalau_ada', true],
   ['-', 'isi_kalau_ada', true] //Ganti jd nomormu sama Namamu
   // [number, dia creator/owner?, dia developer?]
